98739b61ef82f11b59df1f4814895154ada6f30b
Shaun-maker <a.paillaud75@gmail.com>
2022-09-07 22:02:36 +0200

    add some css property

--- vancraft/templates/header/header.php
+++ vancraft/templates/header/header.php
@@ -1,5 +1,5 @@
-<div class="header-log-off">
-    <a href="index.php"><img src="images/logo/logo_xs.svg" alt="logo xs" /></a>
+<div class="header-container-offline">
+    <a href="index.php"><img class="logo-xs" src="images/logo/logo_xs.svg" alt="logo xs" /></a>
     <input type="text" class="search-bar-primary" name="search-bar" placeholder="Rechercher ...">
     <button type="button" class="btn-orange" name="sign-in" >S'inscrire</button>
     <button type="button" class="btn-orange-log-in" name="log-in" >Se connecter</button>
--- vancraft/templates/layout.php
+++ vancraft/templates/layout.php
@@ -14,5 +14,6 @@
     // <?= $content
 
     // <?= $footer ?>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>vv
 </body>
 </html>
