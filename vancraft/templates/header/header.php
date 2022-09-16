e716572e9a5b3e789396c3f031131f37ff9845ef
Shaun-maker <a.paillaud75@gmail.com>
2022-09-16 19:44:23 +0200

    try to adjust search bar

--- vancraft/templates/header/header.php
+++ vancraft/templates/header/header.php
@@ -1,8 +1,8 @@
 <div class="header-container-offline">
     <a class="logo-xs" href="index.php"><img src="images/logo/logo_xs.svg" alt="logo xs" /></a>
     <input type="text" class="search-bar-primary" name="search-bar" placeholder="Rechercher ...">
-    <div class="header-container-button">
-        <button type="button" class="btn-orange-subscribe" name="sign-in" >S'inscrire</button>
-        <button type="button" class="btn-orange-log-in" name="log-in" >Se connecter</button>
-    </div>
+    <!--<div class="header-container-button"> -->
+        <button type="button" class="btn-orange" name="sign-in" ><b>S'inscrire</b></button>
+        <button type="button" class="btn-orange-log-in" name="log-in" ><b>Se connecter</b></button>
+    <!--</div>-->
 </div>
