61dac224eaee16d88f35397861d664e31f2ea54c
Shaun-maker <a.paillaud75@gmail.com>
2022-09-16 14:33:03 +0200

    try flexbox on header nav

--- vancraft/templates/header/header.php
+++ vancraft/templates/header/header.php
@@ -1,6 +1,8 @@
 <div class="header-container-offline">
     <a href="index.php"><img class="logo-xs" src="images/logo/logo_xs.svg" alt="logo xs" /></a>
     <input type="text" class="search-bar-primary" name="search-bar" placeholder="Rechercher ...">
-    <button type="button" class="btn-orange" name="sign-in" >S'inscrire</button>
-    <button type="button" class="btn-orange-log-in" name="log-in" >Se connecter</button>
+    <div class="header-container-button">
+        <button type="button" class="btn-orange" name="sign-in" >S'inscrire</button>
+        <button type="button" class="btn-orange-log-in" name="log-in" >Se connecter</button>
+    </div>
 </div>
