f13888934d4ca6ed33666f05fb0ee3ce73fc88da
Shaun-maker <a.paillaud75@gmail.com>
2022-09-20 21:10:23 +0200

    start adding icons

--- vancraft/templates/header/header.php
+++ vancraft/templates/header/header.php
@@ -1,8 +1,6 @@
 <div class="header-container-offline">
     <a class="logo-xs" href="index.php"><img src="images/logo/logo_xs.svg" alt="logo xs" /></a>
-    <input type="text" class="search-bar-primary" name="search-bar" placeholder="Rechercher ...">
-    <!--<div class="header-container-button"> -->
+    <input type="text" class="search-bar-primary" name="search-bar"  placeholder="Rechercher ...">
         <button type="button" class="btn-orange" name="sign-in" ><b>S'inscrire</b></button>
         <button type="button" class="btn-orange-log-in" name="log-in" ><b>Se connecter</b></button>
-    <!--</div>-->
 </div>
