e4f952e7b4c8f290e5f3950b30e3e0d7f0f5b8bd
Shaun-maker <a.paillaud75@gmail.com>
2022-10-02 21:35:48 +0200

    add h1 title and main logo on homepage

--- vancraft/templates/home/homepage.php
+++ vancraft/templates/home/homepage.php
@@ -1,5 +1,13 @@
 <?php $title = "VanCraft - Page d'accueil"; ?>
 <div class="main">
-    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <div class="container-title">
+        <div class="title-text-container">
+            <h1 class="title-title">VanCraft</h1>
+            <h4 class="title-subtitle">Une aide communautaire à la réalisation de son véhicule aménagé.</h4>
+        </div>
+       <div class="title-logo-container">
+            <img src="images/logo/logo.svg" alt="VanCraft Logo"/>
+       </div>
+    </div>
 </div>
 <?php require('templates/layout.php'); ?>
