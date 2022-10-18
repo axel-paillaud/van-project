e32a1a8494d63eb0871525aac3ade18265720bcc
Shaun-maker <a.paillaud75@gmail.com>
2022-10-18 06:55:05 +0200

    work on subscribe form

--- vancraft/templates/log/subscribe.php
+++ vancraft/templates/log/subscribe.php
@@ -3,7 +3,15 @@
 <?php ob_start(); ?>
 <div class="main">
     <div class="subscribe-container">
-        HELLO 
+        <form action="post">
+            <div><label for="name"><b>Prénom</b></label></div>
+            <div><input class="subscribe-field" type="text" name="name"></div>
+            <div><label for="email"><b>E-mail</b></label></div>
+            <div><input class="subscribe-field" type="email" name="email"></div>
+            <div><label for="password"><b>Mot de passe</b></label></div>
+            <div><input class="subscribe-field" type="password" name="passord"></div>
+            <div><input class="btn-orange" type="submit" value="S'inscrire"></div>
+        </form>
     </div>
 </div>
 <?php $content = ob_get_clean(); 
