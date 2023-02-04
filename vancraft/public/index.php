9b16ca509abbdcbde26d1aa31e66435a37c9a06b
Axel <a.paillaud75@gmail.com>
2023-02-04 18:02:38 +0100

    setup twig template system

--- vancraft/public/index.php
+++ vancraft/public/index.php
@@ -4,6 +4,12 @@ session_start();
 const BASE_PATH = __DIR__ . '/../';
 
 require BASE_PATH . 'functions.php';
+
+//Twig init
+require_once base_path('vendor/autoload.php');
+$loader = new \Twig\Loader\FilesystemLoader(base_path('templates'));
+$twig = new \Twig\Environment($loader); //add cache option here to compile template and load faster
+
 require base_path('src/controllers/header.php');
 require base_path('src/controllers/footer.php');
 include base_path('src/controllers/sidebar.php');
