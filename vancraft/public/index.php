b38de1d6fac6a941fc1b103fa8d638414c119814
Axel <a.paillaud75@gmail.com>
2023-02-05 20:14:35 +0100

    setup twig

--- vancraft/public/index.php
+++ vancraft/public/index.php
@@ -7,7 +7,7 @@ require BASE_PATH . 'functions.php';
 
 //Twig init
 require_once base_path('vendor/autoload.php');
-$loader = new \Twig\Loader\FilesystemLoader(base_path('templates'));
+$loader = new \Twig\Loader\FilesystemLoader(base_path('templates/'));
 $twig = new \Twig\Environment($loader); //add cache option here to compile template and load faster
 
 require base_path('src/controllers/header.php');
