4cf4b654730a993b835bda37e02d3985e3d29a9f
Axel Paillaud <a.paillaud75@gmail.com>
2023-02-08 15:53:32 +0100

    twig migration for homepage OK

--- vancraft/templates/home/homepage.php
+++ vancraft/templates/home/homepage.php
@@ -1,17 +1,15 @@
 {% extends "layout.php" %}
 {% block content %}
 <main class="main">
-    <?php if (isset($message) && !empty($message)) : ?>
-        <?php if ($bad) : ?>
-            <div class="message-container error">
-                <?= $message ?>
-            </div>
-        <?php else : ?>
-            <div class="message-container">
-                <?= $message ?>
-            </div>
-        <?php endif; ?>
-    <?php endif; ?>
+    {% if bad_message %}
+        <div class="message-container error">
+            {{ message }}
+        </div>
+    {% elseif message %}
+        <div class="message-container">
+            {{ message }}
+        </div>
+    {% endif %}
     <div class="title-container">
         <div class="title-text-container">
             <h1 class="title-title">VanCraft</h1>
@@ -32,36 +30,37 @@
         <!-- parent's div for first child and last child -->
         <div>
         <?php foreach ($posts as $post) : ?>
+        {% for post in posts %}
             <div class="post-container">
                 <div class="row-container">
                     <div class="post-title">
-                        <?= $post->title ?>
+                        {{ post.title }}
                     </div>
                     <div class="views-votes-container">
                         <div class="post-stat">
-                            <?= $post->votes ?> votes
+                            {{ post.votes }} votes
                         </div>
                         <div class="post-stat">
-                            <?= $post->answers ?> réponses
+                            {{ post.answers }} réponses
                         </div>
                         <div class="post-stat">
-                            <?= $post->views ?> vues
+                            {{ post.views }} vues
                         </div>
                     </div>
                 </div>
                 <div class="row-container">
                     <div class="tag-container">
-                        <?php foreach ($post->tags as $tag) : ?>
-                            <div class="btn-orange"><?= $tag->title ?></div>
-                        <?php endforeach; ?>
+                        {% for tag in post.tags %}
+                            <div class="btn-orange">{{ tag.title }}</div>
+                        {% endfor %}
                     </div>
                     <div class="user-container">
-                        <img class="profile-picture-small" src="<?= $post->user_image_profile_url ?>" alt="Photo profil">
-                        <b style="margin-left: 8px;" ><?= $post->user_name ?></b>
+                        <img class="profile-picture-small" src="{{ post.user_image_profile_url }}" alt="Photo profil">
+                        <b style="margin-left: 8px;" >{{ post.user_name }}</b>
                     </div>
                 </div>
             </div>
-        <?php endforeach; ?>
+        {% endfor %}
         </div>   
     </div>
 </main>
