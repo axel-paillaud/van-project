5fca863c45569ff003b10905d7bda48cd7f7141f
Axel <a.paillaud75@gmail.com>
2023-02-12 18:16:54 +0100

    start eventListener on input event

--- vancraft/public/assets/js/addTags.js
+++ vancraft/public/assets/js/addTags.js
@@ -4,9 +4,14 @@ let modalTags = document.getElementById("js-modal-tags");
 //init tags = prepare for write tags, remove text information and set the focus on input tags
 const initTags = function(e) {
     modalTags.firstElementChild.innerText = "";
-    console.log(document.querySelector(".input-tag-container input"));
-    document.querySelector(".input-tag-container input").focus();
+    let inputTag = document.querySelector(".input-tag-container input");
+    inputTag.focus();
+    inputTag.addEventListener('input', addTags);
     modalTags.removeEventListener('click', initTags);
+}
+
+const addTags = function(e) {
+    console.log("hello");
 }
 
 fetch(url)
