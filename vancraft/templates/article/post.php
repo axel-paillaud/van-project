5037022d3542e6f642470f177a4a43c76eb97fa7
Axel Paillaud <a.paillaud75@gmail.com>
2023-02-16 16:04:17 +0100

    css/html article page wip

--- vancraft/templates/article/post.php
+++ vancraft/templates/article/post.php
@@ -6,39 +6,41 @@
 
 <main class="main">
     <article class="article-container">
-        <div>
-            <h2 class="margin-16-0 font-normal">Pourquoi le choix de l'armaflex est judicieux face à de la laine de verre</h2>
-            <a class="btn-primary">Poser une question</a>
+        <div class="flex-space-between">
+            <h2 class="margin-16-0 font-normal font-size-27">Pourquoi le choix de l'armaflex est judicieux face à de la laine de verre</h2>
+            <a class="btn-orange">Poser une question</a>
         </div>
-        <small>
+        <small class="flex align-center gap-16">
             <time>Date: Aujourd'hui</time><p>Nombre de vue: 12</p>
         </small>
-        <hr>
-        <div>
-            <div><i class="fa-regular fa-thumbs-up"></i></div>
-            <p>-2</p>
-            <div><i class="fa-solid fa-thumbs-down"></i></div>
-        </div>
-        <div>
-            <pre>
-                Est-ce qu'il y a vraiment un gain de performance d'isolation en partant sur de l'armaflex plutôt que de la laine de verre ?
-                J'ai des difficultées à positionner la laine de verre par endroit (voir photo) et je pense partir sur de l'armaflex pour mon prochain véhicule.
-            </pre>
+        <hr class="margin-16--32 light-mid-gray light-hr">
+        <div class="flex">
             <div>
+                <div class="circle-vote color-di-sierra"><i class="fa-regular fa-thumbs-up"></i></div>
+                <p class="font-size-24">-2</p>
+                <div class="circle-vote color-laser"><i class="fa-solid fa-thumbs-down"></i></div>
+            </div>
+            <div>
+                <pre>
+                    Est-ce qu'il y a vraiment un gain de performance d'isolation en partant sur de l'armaflex plutôt que de la laine de verre ?
+                    J'ai des difficultées à positionner la laine de verre par endroit (voir photo) et je pense partir sur de l'armaflex pour mon prochain véhicule.
+                </pre>
                 <div>
-                    <figure>
-                        <img src="assets/images/tmp/img-1.png" alt="Image utilisateur 1">
-                    </figure>
-                    <ul>
-                        <li>isolation</li>
-                        <li>armaflex</i>
-                        <li>laine de verre</li>
-                    </ul>
-                </div>
-                <div>
-                    <div class="user-container">
-                        <img class="profile-picture-small" src="assets/images/users/default/profile_images/1.jpg" alt="Photo profil">
-                        <b style="margin-left: 8px;" >Jean-luc</b>
+                    <div>
+                        <figure>
+                            <img src="assets/images/tmp/img-1.png" alt="Image utilisateur 1">
+                        </figure>
+                        <ul>
+                            <li>isolation</li>
+                            <li>armaflex</i>
+                            <li>laine de verre</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <div class="user-container">
+                            <img class="profile-picture-small" src="assets/images/users/default/profile_images/1.jpg" alt="Photo profil">
+                            <b style="margin-left: 8px;" >Jean-luc</b>
+                        </div>
                     </div>
                 </div>
             </div>
