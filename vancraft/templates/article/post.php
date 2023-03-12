828607ca1f2c175fefdcf6cf82447aea156381e5
Axel Paillaud <a.paillaud75@gmail.com>
2023-03-12 12:07:42 +0100

    add answer field in post page, add some margin class with tailwind style

--- vancraft/templates/article/post.php
+++ vancraft/templates/article/post.php
@@ -7,13 +7,13 @@
 <main class="main">
     <article class="article-container margin-64-0-32-0">
         <div class="flex-space-between">
-            <h2 class="margin-16-0 font-normal font-size-27">Pourquoi le choix de l'armaflex est judicieux face à de la laine de verre</h2>
-            <a class="btn-orange">Poser une question</a>
+            <h2 class="my-16 ml-0 mr-24 font-normal font-size-27">Pourquoi le choix de l'armaflex est judicieux face à de la laine de verre</h2>
+            <a href="#" class="btn-orange">Poser une question</a>
         </div>
         <small class="flex align-center gap-16">
             <p>Date : <time>Aujourd'hui</time></p><p>Nombre de vue : 12</p><p>Dernière modification : <time>Aujourd'hui</time></p>
         </small>
-        <hr class="margin-16--32-64--32 light-mid-gray light-hr">
+        <hr class="-mx-32 mt-16 mb-64 light-mid-gray light-hr">
         <div class="flex">
             <div class="flex flex-direction-column align-center gap-8 flex-basis-10">
                 <div class="circle-vote background-color-di-sierra"><i class="fa-regular fa-thumbs-up"></i></div>
@@ -52,42 +52,46 @@
         </div>
     </article>
     <p class="margin-16-0">Nombre de réponse : 1</p>
-    <article class="article-container margin-0-0-32-0">
-    <div class="flex">
-        <div class="flex flex-direction-column align-center gap-8 flex-basis-10">
-            <div class="circle-vote background-color-di-sierra"><i class="fa-regular fa-thumbs-up"></i></div>
-            <p class="font-size-24">-2</p>
-            <div class="circle-vote background-color-laser"><i class="fa-solid fa-thumbs-down"></i></div>
-        </div>
-        <div class="padding-0-32 flex-basis-90">
-            <p class="margin-0-0-32-0">
-                Oui, l'armaflex a un pouvoir isolant bien supérieur à la laine de verre, de plus il est fourni "adhésivé",
-                il suffit juste de le coller sur une surface propre. Son inconvénient est le prix, surtout comparé à la
-                laine de verre, qui elle, n'est pas chère du tout.
-            </p>
-            <div class="flex space-between flex-wrap">
-                <div class="flex gap-32 flex-wrap margin-0-0-32-0">
-                    <figure class="img-post">
-                        <img class="full-size cover" src="assets/images/tmp/img-3.jpg" alt="Image utilisateur 1">
-                    </figure>
-                    <figure class="img-post">
-                        <img class="full-size cover" src="assets/images/tmp/img-2.JPG" alt="Image utilisateur 1">
-                    </figure>
-                </div>
-                <div class="flex align-items-end justify-content-end width-100">
-                    <div class="user-container">
-                        <img class="profile-picture-small" src="assets/images/users/default/profile_images/1.jpg" alt="Photo profil">
-                        <b style="margin-left: 8px;" >Jean-luc</b>
+    <article class="article-container mb-32">
+        <div class="flex">
+            <div class="flex flex-direction-column align-center gap-8 flex-basis-10">
+                <div class="circle-vote background-color-di-sierra"><i class="fa-regular fa-thumbs-up"></i></div>
+                <p class="font-size-24">-2</p>
+                <div class="circle-vote background-color-laser"><i class="fa-solid fa-thumbs-down"></i></div>
+            </div>
+            <div class="padding-0-32 flex-basis-90">
+                <p class="margin-0-0-32-0">
+                    Oui, l'armaflex a un pouvoir isolant bien supérieur à la laine de verre, de plus il est fourni "adhésivé",
+                    il suffit juste de le coller sur une surface propre. Son inconvénient est le prix, surtout comparé à la
+                    laine de verre, qui elle, n'est pas chère du tout.
+                </p>
+                <div class="flex space-between flex-wrap">
+                    <div class="flex gap-32 flex-wrap margin-0-0-32-0">
+                        <figure class="img-post">
+                            <img class="full-size cover" src="assets/images/tmp/img-3.jpg" alt="Image utilisateur 1">
+                        </figure>
+                        <figure class="img-post">
+                            <img class="full-size cover" src="assets/images/tmp/img-2.JPG" alt="Image utilisateur 1">
+                        </figure>
+                    </div>
+                    <div class="flex align-items-end justify-content-end width-100">
+                        <div class="user-container">
+                            <img class="profile-picture-small" src="assets/images/users/default/profile_images/1.jpg" alt="Photo profil">
+                            <b style="margin-left: 8px;" >Jean-luc</b>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
     </article>
+        <form>
+            <div class="article-container">
+                <label class="font-size-20" for="answer">Votre réponse</label>
+                <hr class="light-hr light-mid-gray -mx-32 my-16">
+                <textarea id="answer" class="m-w-full w-full min-h-200" required autocomplete="off" name="answer"></textarea>
+            </div>
+            <input id="submit-answer" class="btn-orange mt-16 mb-32" type="submit" value="Poster la réponse">
+        </form>
 </main>
-
-<br><br><br><br><br><br><br><br><br><br><br><br>
-<br><br><br><br><br><br><br><br><br><br><br><br>
-<br><br><br><br><br><br><br><br><br><br><br><br>
 
 {% endblock %}
