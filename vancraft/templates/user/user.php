d088667c43f2541ea1972aa2b79c6d996bbf5908
Axel Paillaud <a.paillaud75@gmail.com>
2023-03-16 15:55:40 +0100

    add about and stats section

--- vancraft/templates/user/user.php
+++ vancraft/templates/user/user.php
@@ -18,5 +18,34 @@
         </figure>
         <a class="btn-orange px-16 py-10" href="#"><i class="fa-solid fa-pen text-black mr-8"></i>Editer le profil</a>
     </div>
+    <!--stats and about section-->
+    <div class="m-32 flex gap-32">
+        <!--stats section-->
+        <div class="basis-20">
+            <div class="font-24 mb-8">Statistiques</div>
+            <div class="about-container flex justify-around">
+                <div class="flex flex-col justify-around">
+                    <span>3 réponses</span>
+                    <span>3 questions</span>
+                </div>
+                <!--if we need more user stats, we can put 2 mores here-->
+                <div class="display-none">
+                    <span class="hidden">32 médailles</span>
+                    <span class="hidden">empty</span>
+                </div>
+            </div>
+        </div>
+        <!--about section-->
+        <div class="basis-80">
+            <div class="font-24 mb-8">À propos</div>
+            <div  class="about-container flex align-center">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est sunt obcaecati qui, nihil aspernatur a in 
+                illo doloribus! Provident delectus ut autem recusandae? Error obcaecati amet eveniet iusto, nihil tempora?
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est sunt obcaecati qui, nihil aspernatur a in 
+                illo doloribus! Provident delectus ut autem recusandae? Error obcaecati amet eveniet iusto, nihil tempora?
+            </div>
+        </div>
+    </div>
+    <br>
 </main>
 {% endblock %}
